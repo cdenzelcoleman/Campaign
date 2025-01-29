da2258c0683c2433b0b3c8ade9f985a55447c347
cdenzelcoleman <camdcole@gmail.com>
2025-01-29 02:10:56 -0600

    CampaignDetailPage Info Finish

---
+++ frontend/src/pages/CampaignDetailPage/CampaignDetailPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CampaignDetail from '../../components/CampaignDetail/CampaignDetail';
+import NavBar from '../../components/NavBar/NavBar';
+
+const CampaignDetailPage = () => {
+    return (
+        <>
+        <NavBar />
+        <CampaignDetail />
+        </>
+    );
+    };
+
+export default CampaignDetailPage;
