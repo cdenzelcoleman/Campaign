64902e6aaa4e1351f3126e03bb97c44b4a1827c3
cdenzelcoleman <camdcole@gmail.com>
2025-01-29 01:08:05 -0600

    ProtectedRoutes

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { getUser } from '../services/authService';
+import { getCurrentUser, getToken, logout } from '../services/authService';
 
 export const AuthContext = createContext();
 
